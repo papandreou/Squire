73966ad6b0f8933eefc22b112d406dad82d29027
Gert K. Sønderby <gert@one.com>
2015-10-01 14:30:46 +0200

    WIP: Fix for pasting into PREs.

--- build/squire-raw.js
+++ build/squire-raw.js
@@ -3405,7 +3405,7 @@ var decreaseListLevel = function ( frag ) {
     return frag;
 };
 
-var makePreformatted = function ( frag ) {
+var getTextFromHTMLFragment = function ( self, frag ) {
     var walker = getBlockWalker( frag ),
         lines = [],
         node;
@@ -3413,10 +3413,13 @@ var makePreformatted = function ( frag ) {
         // Strip down to text only
         lines.push( node.textContent );
     }
-    node = this._doc.createTextNode( lines.join( '\n' ) || '\n' );
+    return self._doc.createTextNode( lines.join( '\n' ) || '\n' );
+};
+
+var makePreformatted = function ( frag ) {
     return this.createElement( 'PRE',
         this._config.tagAttributes.pre, [
-            node
+            getTextFromHTMLFragment( this, frag )
         ] );
 };
 
@@ -3689,13 +3692,14 @@ proto.insertHTML = function ( html, isPaste ) {
         frag = this._doc.createDocumentFragment(),
         div = this.createElement( 'DIV' );
 
-    if ( this.hasFormat( 'pre' ) || this.hasFormat( 'code' ) ) {
+    // Parse HTML into DOM tree
+    div.innerHTML = html;
+    frag.appendChild( empty( div ) );
+    // If range is entirely inside PRE tag
+    debugger;
+    if ( getNearest( range.commonAncestorContainer, 'PRE' ) ) {
         // Insert unparsed in text node
-        frag.appendChild( this._doc.createTextNode( html ) );
-    } else {
-        // Parse HTML into DOM tree
-        div.innerHTML = html;
-        frag.appendChild( empty( div ) );
+        replaceWith( div, getTextFromHTMLFragment( this, frag ) );
     }
 
     // Record undo checkpoint
@@ -3744,7 +3748,7 @@ proto.insertHTML = function ( html, isPaste ) {
 };
 
 proto.insertPlainText = function ( plainText, isPaste ) {
-    if ( this.hasFormat( 'pre' ) || this.hasFormat( 'code' ) ) {
+    if ( getNearest( range.commonAncestorContainer, 'PRE' ) ) {
         return this.insertHTML( plainText, isPaste );
     } else {
         var lines = plainText.split( '\n' ),
